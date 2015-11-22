1716835425916f67b341af34f752db7c2543d44b
Alvaro Prieto <apg88zx@gmail.com>
2015-11-22 10:33:24 -0800

    Adding simple LED blinky python script

---
+++ sw/blinky.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+#
+# Blink some lights, but with Python! (and the STM32F407 discovery board)
+#
+
+import sys
+import time
+from bridge import Bridge
+
+BLUE_LED = 'PD15'
+
+if len(sys.argv) < 2:
+	print 'Usage: ', sys.argv[0], '/path/to/serial/device'
+	sys.exit()
+
+stream_file = sys.argv[1]
+
+bridge = Bridge(stream_file)
+
+# Enable the blue LED
+bridge.gpiocfg(BLUE_LED, 'output')
+
+for dummy in range(25):
+	time.sleep(0.05)
+	bridge.gpio(BLUE_LED, 1)
+	time.sleep(0.05)
+	bridge.gpio(BLUE_LED, 0)
+
+bridge.close()
