9d1c9ba4f95e251b2aacbe30482eaa80ba32b925
Alvaro Prieto <apg88zx@gmail.com>
2015-11-22 10:29:26 -0800

    Adding GPIO config support

--- sw/bridge.py
+++ sw/bridge.py
@@ -4,10 +4,23 @@
 #
 
 import serial
+import string
 import sys
 import re
 
 class Bridge:
+	pinModes = {
+		'input': 'in',
+		'output': 'outpp',
+		'output-od': 'outod'
+	}
+
+	pullModes = {
+		'up': 'pullup',
+		'down': 'pulldown',
+		'none': 'nopull'
+	}
+
 	def __init__(self, serial_device):
 		self.stream = None
 
@@ -43,6 +56,37 @@ class Bridge:
 			r_bytes = int(result[1])
 
 		return r_bytes
+
+	def gpiocfg(self, name, mode='input', pull=None):
+		m = re.search('[Pp]([A-Ea-e])([0-9]+)', name)
+
+		if m is None:
+			raise ValueError('Invalid gpio name. Should be of the form PX.Y where X is A-E and Y is 0-15')	
+
+		port = m.group(1)
+		pin = int(m.group(2))
+
+		if pin > 15:
+			raise ValueError('Invalid pin. Should be a number from 0-15')
+
+		if mode not in self.pinModes:
+			raise ValueError('Invalid pin mode. Valid modes: <' + string.join(self.pinModes.keys(), '|') + '>')
+
+		if pull != None and pull not in self.pullModes:
+			raise ValueError('Invalid pull mode. Valid modes: <' + string.join(self.pullModes.keys(), '|') + '>')			
+
+		cmd = 'gpiocfg ' + port + ' ' + str(pin) + ' ' + self.pinModes[mode]
+
+		if pull != None:
+			cmd += ' ' + self.pullModes[pull]
+
+		self.stream.write(cmd + '\n')
+
+		line = self.stream.readline()
+		result = line.strip().split(' ')
+
+		if result[0] != 'OK':
+			print("Error configuring pin")
 
 	def gpio(self, name, value = None):
 		m = re.search('[Pp]([A-Ea-e])([0-9]+)', name)
