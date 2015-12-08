ab6cdf07e0c35188a6eda982fde43edab1ca9679
Alvaro Prieto <apg88zx@gmail.com>
2015-12-07 18:12:32 -0800

    Eek, I forgot the include guard!

--- fw/adc.h
+++ fw/adc.h
@@ -1,6 +1,11 @@
+#ifndef __ADC_H__
+#define __ADC_H__
+
 #include <stdint.h>
 #include <stdbool.h>
 
 int32_t adcInit();
 int32_t adcGetPin(GPIO_TypeDef *port, uint8_t pin);
 int32_t adcRead(uint8_t adc);
+
+#endif
