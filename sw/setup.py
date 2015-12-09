9bab5726ed3e603998d90e62a6cd231e8e509f18
Alvaro Prieto <apg88zx@gmail.com>
2015-12-08 20:51:21 -0800

    Bumping version

--- sw/setup.py
+++ sw/setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.5',
+    version='0.0.6',
 
     description='A Python to STM32F4 Bridge Project',
     long_description=long_description,
