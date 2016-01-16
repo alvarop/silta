4c7160ffac766f95061052f865a2afada68eb5e8
Alvaro Prieto <apg88zx@gmail.com>
2016-01-16 10:14:22 -0800

    Fix blinky example

--- sw/examples/blinky.py
+++ sw/examples/blinky.py
@@ -8,7 +8,7 @@ import sys
 import time
 from silta import stm32f407
 
-BLUE_LED = 'PD15'
+GREEN_LED = 'PD12'
 
 if len(sys.argv) < 2:
     print('Usage: ' + sys.argv[0] + '/path/to/serial/device')
@@ -19,12 +19,12 @@ stream_file = sys.argv[1]
 bridge = stm32f407.bridge(stream_file)
 
 # Enable the blue LED
-bridge.gpiocfg(BLUE_LED, 'output')
+bridge.gpiocfg(GREEN_LED, 'output')
 
 for dummy in range(25):
     time.sleep(0.05)
-    bridge.gpio(BLUE_LED, 1)
+    bridge.gpio(GREEN_LED, 1)
     time.sleep(0.05)
-    bridge.gpio(BLUE_LED, 0)
+    bridge.gpio(GREEN_LED, 0)
 
 bridge.close()
